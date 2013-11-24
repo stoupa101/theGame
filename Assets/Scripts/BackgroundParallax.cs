2a090fe1efe358ef9dcba9a2c53b8b52187e77fd
stoupa101 <stoupa101@seznam.cz>
2013-11-24 14:56:21 +0100

    Prvni naliti

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundParallax : MonoBehaviour {
+
+	public Transform[] backgrounds;			//Pole vsech pozadi, ktere budou paralaxovany
+	public float parallaxScale;				//Proporce mezi pohybem kamery a pohybem pozadi.
+	public float parallaxReductionFactor;	//
+	public float smoothing;					//Jak hladky bude paralax efekt
+
+	private Transform cam;					//odkaz na pozici hlavni kamery
+	private Vector3 previousCamPos;			//pozice kamery v predeslem framu
+
+
+	void Awake () {
+		cam = Camera.main.transform;
+	}
+
+	void Start () {
+		//pri startu je predchozi pozice aktualni
+		previousCamPos = cam.position;
+	}
+
+	void Update () {
+		float parallax = (previousCamPos.x - cam.position.x) * parallaxScale;
+		for (int i = 0; i < backgrounds.Length; i++) {
+			float backgroundTargetPosX = backgrounds[i].position.x + parallax * (i * parallaxReductionFactor + 1);
+			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+		}
+		previousCamPos = cam.position;
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	[HideInInspector]
+	public bool facingRight = true;
+
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+
+	private float rndWait = 1f;
+	private Animator anim;
+
+	void Awake () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Start() 
+	{
+		StartCoroutine (GiveRandomWait());
+	}
+	
+	void Update () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+		if (Mathf.Abs(h) == 0) {
+			anim.SetInteger("Wait", (int) rndWait);
+		}
+
+		anim.SetFloat ("Speed", Mathf.Abs (h));
+
+		if ( h * rigidbody2D.velocity.x < maxSpeed ) {
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		}
+
+		if ( Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed ) {
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+		if (h > 0 && !facingRight) {
+				Flip ();
+		} else if (h < 0 && facingRight) {
+				Flip ();
+		}
+
+	}
+
+	void Flip() 
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	IEnumerator GiveRandomWait()
+	{
+		while(true)
+		{
+			rndWait = Random.Range (1, 3);
+			yield return new WaitForSeconds(10);
+		}
+	}
+}
